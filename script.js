4d3b661d56266847eff6c4e47f5d0146c96f871a
sejlok <cashan007@gmail.com>
2025-03-24 20:46:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript for Mobile Navigation Toggle
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+mobileMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
